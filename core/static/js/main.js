55b29b200177e34c4879d02ef45cf3e3f9fdeb7b
Faeze Salehi <112781265+faz-7@users.noreply.github.com>
2025-04-21 13:39:46 +0330

    Update main.js

--- core/static/js/main.js
+++ core/static/js/main.js
@@ -1,1 +1,55 @@
+function insertRecord() {
+  const data = {
+    ssn: document.getElementById("insertSSN").value,
+    firstName: document.getElementById("insertFirstName").value,
+    lastName: document.getElementById("insertLastName").value,
+    specialty: document.getElementById("insertSpecialty").value,
+    yearsOfExperience: document.getElementById("insertYearsOfExperience").value,
+    phoneNum: document.getElementById("insertPhoneNum").value,
+  };
+
+  fetch("/doctor/insert/", {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => alert(data.message))
+    .catch((error) => console.error("Error:", error));
+}
+
+function updateRecord() {
+  const data = {
+    ssn: document.getElementById("updateSSN").value,
+    firstName: document.getElementById("updateFirstName").value,
+    lastName: document.getElementById("updateLastName").value,
+    specialty: document.getElementById("updateSpecialty").value,
+    yearsOfExperience: document.getElementById("updateYearsOfExperience").value,
+    phoneNum: document.getElementById("updatePhoneNum").value,
+  };
+
+  fetch("/doctor/update/", {
+    method: "PUT",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => alert(data.message))
+    .catch((error) => console.error("Error:", error));
+}
+
+function deleteRecord() {
+  const data = {
+    ssn: document.getElementById("deleteSSN").value,
+  };
+
+  fetch("/doctor/delete/", {
+    method: "DELETE",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data),
+  })
+    .then((response) => response.json())
+    .then((data) => alert(data.message))
+    .catch((error) => console.error("Error:", error));
+}
 
